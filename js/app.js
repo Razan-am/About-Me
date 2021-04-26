b18cb99de032c1b866f63a944fd9f177b4770c09
razan-am <razanalamleh@gmail.com>
2021-04-26 17:12:09 +0300

    giving more details to the user about my educational history by question1

--- js/app.js
+++ js/app.js
@@ -9,9 +9,40 @@ function myFunction (){
 
     let userName = prompt('please enter yout name');
   console.log (userName);
-  document.write ('Hello'+" "+userName+','+"welcome to my webpage,let's make you know me better");
+  document.write ('Hello'+" "+userName+','+"welcome to my webpage,let's make you know me better"+
+  "/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
 
-}
+  let question1 = prompt('Would you like to have a brief about my educational history?(YES/NO)').toUpperCase();
+  switch(question1){
+      case 'YES': 
+
+      alert ("Your answer is YES");
+      console.log ('the answer is YES');
+      document.write ('Well thanks for your interesting, i was majoring in the college in the architecture field. And now im building my self to become a software developer.'
+      "/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////");
+      break;
+
+      case 'NO':
+
+        alert ("Your answer is NO");
+        console.log ('the answer is NO');
+        break;
+
+    default:
+        alert ('this is not the right answer');
+        console.log('this is not the right answer');
+        break;
+
+
+    
+
+
+
+  }
+
+
+
+
 
 
 

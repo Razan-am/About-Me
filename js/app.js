7d660697c0d0a936455d32323c633a0d02ab9cf1
razan-am <razanalamleh@gmail.com>
2021-04-27 22:13:14 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//201d2
+
 let start = document.getElementById("button")
 
   .onclick = function () { greeting() };
@@ -11,127 +11,127 @@ let start = document.getElementById("button")
 function greeting(questions) {
 
 
-  // alert('hello');
+  alert('hello');
 
-  // let userName = prompt('please enter yout name');
-  // console.log(userName);
-  // document.write('<p>Hello' + " " + userName + ',' + "welcome to my webpage,let's make you know me better.</p><br>");
-
-
-
-  // let question1 = prompt('Would you like to have a brief about my educational history?(YES/NO)').toUpperCase();
-
-  // switch (question1) {
-  //   case 'YES' || 'Y':
-
-  //     alert("Your answer is YES");
-  //     console.log('the answer is YES');
-  //     document.write ('<p>Well thanks for your interesting, i was majoring in the college in the architecture field. And now im building my self to become a software developer.</p><br>');
-  //     break;
-
-  //   case 'NO' || 'N':
-
-  //     alert("Your answer is NO");
-  //     console.log('the answer is NO');
-  //     break;
-
-  //   default:
-  //     alert('this is not the right answer');
-  //     console.log('this is not the right answer');
-  //     break;
-  // }
+  let userName = prompt('please enter yout name');
+  console.log(userName);
+  document.write('<p>Hello' + " " + userName + ',' + "welcome to my webpage,let's make you know me better.</p><br>");
 
 
 
-  // let question2 = prompt('Would you like to have an overview of my job experience?(yes/no)').toLowerCase();
+  let question1 = prompt('Would you like to have a brief about my educational history?(YES/NO)').toUpperCase();
 
-  // switch (question2) {
-  //   case 'yes' || 'y':
+  switch (question1) {
+    case 'YES' || 'Y':
 
-  //     alert("Your answer is yes");
-  //     console.log('the answer is yes');
-  //     document.write ('<p>So i have many different job experiences in different fields,i have worked in the sales/marketing/promotion,also as a event/parties planner, and i have worked as adesigner espacially as a graphic designer.</p><br>');
-  //     break;
+      alert("Your answer is YES");
+      console.log('the answer is YES');
+      document.write ('<p>Well thanks for your interesting, i was majoring in the college in the architecture field. And now im building my self to become a software developer.</p><br>');
+      break;
 
-  //   case 'NO' || 'N':
+    case 'NO' || 'N':
 
-  //     alert("Your answer is NO");
-  //     console.log('the answer is NO');
-  //     break;
-  // }
+      alert("Your answer is NO");
+      console.log('the answer is NO');
+      break;
 
-
-  // let question3 = prompt('Now as you know a little bit more about me , could you tell if i am ok with changes or not ?(yes/no)').toLowerCase();
-
-  // switch (question3) {
-  //   case 'yes' || 'y':
-
-  //     alert("Your answer is yes, and it is a correct answer ");
-  //     console.log('the answer is yes, and it is a correct answer ');
-  //     break;
-
-  //   case 'no' || 'n':
-
-  //     alert("Your answer is NO");
-  //     console.log('the answer is NO');
-  //     break;
-  // }
+    default:
+      alert('this is not the right answer');
+      console.log('this is not the right answer');
+      break;
+  }
 
 
 
-  // let question4 = prompt('Would you like to have an idea about my main goal?(YES/No)').toUpperCase;
+  let question2 = prompt('Would you like to have an overview of my job experience?(yes/no)').toLowerCase();
 
-  // switch (question4) {
-  //   case 'YES' || 'Y':
+  switch (question2) {
+    case 'yes' || 'y':
 
-  //     alert("Your answer is YES");
-  //     console.log('the answer is YES');
-  //     document.write('<p>my main goal is to live my life without any regrets and to always do my best in everything</p><br>');
-  //     break;
+      alert("Your answer is yes");
+      console.log('the answer is yes');
+      document.write ('<p>So i have many different job experiences in different fields,i have worked in the sales/marketing/promotion,also as a event/parties planner, and i have worked as adesigner espacially as a graphic designer.</p><br>');
+      break;
 
-  //   case 'NO' || 'N':
+    case 'NO' || 'N':
 
-  //     alert("Your answer is NO");
-  //     console.log('the answer is NO');
-  //     break;
-  // }
-
-  // let question5 = prompt('Do you have a similar goals as mine?(yes/no)').toLowerCase;
-
-  // switch (question5) {
-  //   case 'yes' || 'y':
-
-  //     alert("Your answer is yes");
-  //     console.log('the answer is yes');
-  //     document.write('<p>that is amazing to know</p>');
-  //     break;
-
-  //   case 'no' || 'n':
-
-  //     alert("Your answer is NO");
-  //     console.log('the answer is NO');
-  //     document.write('<p>that is good to know</p>'
-  //     );
-  //     break;
-  // }
+      alert("Your answer is NO");
+      console.log('the answer is NO');
+      break;
+  }
 
 
-  //201d3
+  let question3 = prompt('Now as you know a little bit more about me , could you tell if i am ok with changes or not ?(yes/no)').toLowerCase();
 
-  // let question6 = parseInt(prompt('Can you guess how old i am ?'));
+  switch (question3) {
+    case 'yes' || 'y':
 
-  // let flag = false;
-  // let age = 24;
+      alert("Your answer is yes, and it is a correct answer ");
+      console.log('the answer is yes, and it is a correct answer ');
+      break;
 
-  // for (let i = 1; i < 5; i++){
+    case 'no' || 'n':
+
+      alert("Your answer is NO");
+      console.log('the answer is NO');
+      break;
+  }
 
 
-  //   if (question6 < age) {
-  //     alert('Your answer is too low');
 
-  //   } else (question6 > age);{
-  //     alert('Your answer is too high');
-  //   }
+  let question4 = prompt('Would you like to have an idea about my main goal?(YES/No)').toUpperCase;
+
+  switch (question4) {
+    case 'YES' || 'Y':
+
+      alert("Your answer is YES");
+      console.log('the answer is YES');
+      document.write('<p>my main goal is to live my life without any regrets and to always do my best in everything</p><br>');
+      break;
+
+    case 'NO' || 'N':
+
+      alert("Your answer is NO");
+      console.log('the answer is NO');
+      break;
+  }
+
+  let question5 = prompt('Do you have a similar goals as mine?(yes/no)').toLowerCase;
+
+  switch (question5) {
+    case 'yes' || 'y':
+
+      alert("Your answer is yes");
+      console.log('the answer is yes');
+      document.write('<p>that is amazing to know</p>');
+      break;
+
+    case 'no' || 'n':
+
+      alert("Your answer is NO");
+      console.log('the answer is NO');
+      document.write('<p>that is good to know</p>'
+      );
+      break;
+  }
+
+
+  
+
+  let question6 = parseInt(prompt('Can you guess how old i am ?'));
+
+  let flag = false;
+  let age = 24;
+
+  for (let i = 1; i < 5; i++){
+
+
+    if (question6 < age) {
+      alert('Your answer is too low');
+
+    } else (question6 > age);{
+      alert('Your answer is too high');
+    }
 
 
 
